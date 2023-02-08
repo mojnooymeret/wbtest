b7cb74364fcbfa0fefc5932e7ebb481845239870
mojnooymeret <95657364+mojnooymeret@users.noreply.github.com>
2023-02-09 02:52:30 +0500

    Add files via upload

---
+++ OzonWb/Source.cs
@@ -0,0 +1,15 @@
+﻿namespace OzonWb
+{
+   public class Source
+   {
+      public string url { get; set; }
+      public string price { get; set; }
+      public string service { get; set; }
+      public Source(string url, string price, string service)
+      {
+         this.url = url;
+         this.price = price;
+         this.service = service;
+      }
+   }
+}
